1ae64270a9b2604990ac316390455452246fc307
Massimo-Capurro-Llado <massimocapurrollado@gmail.com>
2021-11-19 10:53:18 +0100

    Fixed the formatting of the result dataframe and some minor fixes

--- main.py
+++ main.py
@@ -51,10 +51,23 @@ def SaveJsonFile(file, data):
     try: 
         data.to_json(file)
     except:
-        print('Something went wrong during the creation of the new file!')
+        print('Something went wrong during the creation of the new Json file!')
         sys.exit()
   
 
+# =============================================================================
+# 
+#Fuction that saves data onto a new Excel file 
+#
+# def SaveExelFile(file, data):
+#     try: 
+#         data.to_exel(file)
+#     except:
+#         print('Something went wrong during the creation of the new Excel file!')
+#         sys.exit()
+# 
+# =============================================================================
+        
 #This function receives 2 strings that represent 2 dates and calculates the distance
 def CalculateDateDistance (str1, str2):
      date0= pd.to_datetime(str1,format="%d/%m/%Y %H:%M")
@@ -64,46 +77,54 @@ def CalculateDateDistance (str1, str2):
 
 #TODO: Try to fix formatting for the date_distance. The result is in sec but it would be nice to keep the TimeDelta format
 def UserFeatures (log_list):
+    
     #Rename the columns for readability
     log_list.rename(columns={ 0:'DATE', 1: 'USER_ID', 3: 'EVENT_TYPE'}, inplace=True)
+    
     #Splitting the DataFrame of logs into groups based on user_id 
     df_per_user = log_list.groupby(log_list.USER_ID)
+    
     #Creating a range for the loop. The max value is the biggest ID 
     users= range(1, log_list.USER_ID.max()+1) 
+    
     features=[]   
     for i in users:
         #Taking as current the DataFrame that contains only the logs of the i-th user
         current= df_per_user.get_group(i)
-    
-        event_participations= current['USER_ID'].value_counts()
-        event_counts= current['EVENT_TYPE'].value_counts()
+        
+        event_participations= current['USER_ID'].value_counts().tolist()
+        event_counts= current['EVENT_TYPE'].value_counts().to_frame()
         print(event_counts, '\n\n')
     
         first_event_date= current['DATE'].min()
-    
         last_event_date= current['DATE'].max()
-    
         dates_distance= CalculateDateDistance(first_event_date, last_event_date)
         print(dates_distance, '\n\n')
-        feature_obj=[i, event_participations, event_counts, first_event_date, last_event_date, dates_distance]
+        
+        feature_obj=[i, event_participations[0], event_counts, first_event_date, last_event_date, dates_distance]
         features.append(feature_obj)
 
     #TODO: Divide in some way lines and columns of the result file (the dataframe shape should be ok)
     #Converting the list of features into a DataFrame
     user_features= pd.DataFrame(features)
     user_features.rename(columns={ 0:'USER_ID', 1: 'Event participation', 2: 'Event type count', 
-                              3: 'First event date', 4:'Last event date', 5:'Date distance first and last event'},
-                     inplace=True)
+                              3: 'First event date', 4:'Last event date', 
+                              5:'Date distance first and last event'}, inplace=True)
     return user_features
 
 #TODO: The real file
 jsonfile = 'indata\logs_Fondamenti di informatica [20-21]_20211103-1845_anonymized.json'  
 
-#TODO: Fix parsing so everyone can try with a smaller file (using head i can take only the first few logs)
-#TODO: Fake file with only the first few logs for simplicity
-#jsonfile = 'indata\logs_analizza1.json' 
-log_list=ReadJsonFile(jsonfile) #log_list is alerady a DataFrame
+# =============================================================================
+# 
+# #TODO: Fake file with only the first few logs for simplicity
+#jsonfile = 'indata\logs_analizza1.json'  
+# 
+# =============================================================================
 
+log_list=ReadJsonFile(jsonfile)
 user_features= UserFeatures(log_list)
-SaveJsonFile(r'indata/User_features.json', user_features)       
+print(user_features.loc[:, 'USER_ID':'Event type count'])
+SaveJsonFile(r'indata/User_features.json', user_features)   
+#SaveExelFile(r'indata/User_features.xlsx', user_features)   
 print("Task ended")
