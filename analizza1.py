507a64b3cef252140a82e88c11522ec8780cd3b1
Vadilonga00 <francyvadi@gmail.com>
2021-11-19 17:02:23 +0100

    miglioramento commenti

--- analizza1.py
+++ analizza1.py
@@ -24,15 +24,13 @@ per utente
 
 import pandas as pd
 
-
-#leggo il file json ridotto logs.json"
-
-df = pd.read_json('/Users/francyvadi/Desktop/cartella senza titolo 3/analizza_log_21-22/indata/logs_Fondamenti di informatica [20-21]_20211103-1845_anonymized.json')
-
 #creo un DataFrame con il file json"
+df = pd.read_json('/Users/francyvadi/Desktop/analizza_log_21-22/indata/logs_Fondamenti di informatica [20-21]_20211103-1845_anonymized.json')
+
 
 #indicizzo i dati "
 df.columns = ['DATA', 'MATRICOLA', 'CORSO', 'COMPONENTE','EVENTO','DESCRIZIONE','ORIGINE','INDIRIZZO']
+
 
 
 #creo un nuvo dataframe prendendo le series MATRICOLA e EVENTO e separo gli eventi
@@ -45,23 +43,22 @@ df3.columns = ['EVENTI COMPIUTI']
 df3=df3['EVENTI COMPIUTI'].str.split('|', expand=True)
 
 
-
+#creo un nuovo dataframe 'eventi_utente' partendo da df3 per analizzarele features sugli eventi
 #quante volte si è verificato ciascun evento  per ogni matricola 
 eventi_utente=df3.apply(pd.Series.value_counts, axis=1).fillna(0)
 
-
-
-
-#la somma di ogni evento per ogni matricola cioè sulle righe
+#su questo nuovo database calcolo la somma di ogni evento per ogni matricola cioè sulle righe
 eventi_utente.sum(axis=1)
 
-#inserisco una nuova colonna con la somma del numero totale di eventi 
+#inserisco una nuova colonna nel quale è presente la somma del numero totale di eventi 
 eventi_utente.insert(0, "somma_tot", eventi_utente.sum(axis=1), allow_duplicates=False)
 
- 
+
+
+#nel database originario specifico la divisione della dta
 df['DATA']=pd.to_datetime(df['DATA'],format="%d/%m/%Y %H:%M")
 
-# creo dataframe massimo e minimo evento per utente
+# creo un dict nel quale è presente massimo e minimo evento 
 massimo=max(df['DATA'])
 minimo=min(df['DATA'])
 
@@ -79,12 +76,14 @@ date_max_min
 delta=massimo - minimo
 print(delta.days)
 
-      
+ 
 
+     
+#intoduco un dict con i giorni della settimana 
 dweek = {0:'Lunedi', 1:'Martedi', 2: 'Mercoledi', 3:'Giovedi', 4:'Venerdi', 5:'Sabato', 6:'Domenica'}
 
 
-
+#uso weekday() per associare ad ogni data il giorno della settimana corrispondente
 df['DATA'][0].weekday()
 dweek[df['DATA'][0].weekday()]
 
