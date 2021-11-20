70d421a1e6faaa7d39c1bbeadbd947ee8cbd2dfd
Giulio Iannello <g.iannello@unicampus.it>
2021-11-20 09:19:19 +0100

    Modificato i nomi delle funzioni per renderli più espliciti Corretto typos

--- analizza_log.py
+++ analizza_log.py
@@ -3,6 +3,7 @@
 Created on Thu Nov 11 23:41:04 2021
 
 @author: carpy
+
 Program to read a list of anonymized logs from a file 
 and calculate a feature vector for each user by saving the data in json format
 Expected json data: a list of log with the following fields:
@@ -64,82 +65,83 @@ def write_json_file(data, file_name, indnt=3):
         fatal_error(message)
         
 
-def tot_occurrences(log_list):
+def add_tot_occurrences(log_list):
     '''
-     calculates the total occurences per user from the list of logs
+     calculates the total occurrences per user from the list of logs
     :param log_list: a list of logs
     :return: a dictionary of dictionary with user codes
-             and total occurences per user 
+             and total occurrences per user
     '''
-    tab_tot_occurences = {}
+    tab_tot_occurrences = {}
     for log in log_list:
-        if not log[1] in tab_tot_occurences:
-            tab_tot_occurences[(log[1])]= {}
-            tab_tot_occurences[(log[1])]['tot_occurences']=1
+        if not log[1] in tab_tot_occurrences:
+            tab_tot_occurrences[(log[1])]= {}
+            tab_tot_occurrences[(log[1])]['tot_occurrences'] = 1
         else:
-            tab_tot_occurences[log[1]]['tot_occurences']+=1
-    return dict(sorted(tab_tot_occurences.items()))
+            tab_tot_occurrences[log[1]]['tot_occurrences'] += 1
+    return dict(sorted(tab_tot_occurrences.items()))
 
 
-def event_occurences(log_list, tab_tot_occurences):
+def add_event_occurrences(log_list, tab_tot_occurrences):
     '''
     calculates the occurences per events
     :param log_list: a list of logs
-    :param tab_tot_occurences: dictionary of dictionary with user codes
-             and total occurences per user
+    :param tab_tot_occurrences: dictionary of dictionary with user codes
+             and total occurrences per user
     :return: the same dictionary adding the
              occurrences for each event
     '''
     
     for log in log_list:
-        if log[4] not in tab_tot_occurences[log[1]]:
-            tab_tot_occurences[log[1]][log[4]]=1
+        if log[4] not in tab_tot_occurrences[log[1]]:
+            tab_tot_occurrences[log[1]][log[4]] = 1
         else:
-            tab_tot_occurences[log[1]][log[4]]+=1
+            tab_tot_occurrences[log[1]][log[4]] += 1
     eventi = set()
-    for user in tab_tot_occurences:
-        eventi = eventi.union(set(tab_tot_occurences[user].keys()))
-    eventi.remove('tot_occurences')
-    for user in tab_tot_occurences:
+    for user in tab_tot_occurrences:
+        eventi = eventi.union(set(tab_tot_occurrences[user].keys()))
+    eventi.remove('tot_occurrences')
+    for user in tab_tot_occurrences:
         for e in eventi:
-            if e not in tab_tot_occurences[user]:
-                tab_tot_occurences[user][e] = 0
-    return tab_tot_occurences
+            if e not in tab_tot_occurrences[user]:
+                tab_tot_occurrences[user][e] = 0
+    return tab_tot_occurrences
 
-def dates_of_occurences(tab_tot_occurences):
+
+def get_dates_of_occurrences(tab_tot_occurrences):
     '''
     calculates all dates on which the user performs an event
-    :param tab_tot_occurences: dictionary of dictionary with user codes,
-             total occurences per user and occurences for each event
+    :param tab_tot_occurrences: dictionary of dictionary with user codes,
+             total occurrences per user and occurences for each event
     :return: the same dictionary with all dates on which the user performs an event
     '''
     dates = {}
-    for user in tab_tot_occurences:
+    for user in tab_tot_occurrences:
         dates[user] = []
         for log in log_list:
             data = log[0].split()
             if log[1] == user:
                 dates[user] += [data[0]]
     return dates
-#It can be useful for the next calculation of first date, last date etc.
+# It can be useful for the next calculation of first date, last date etc.
 
 
-def x(tab_tot_occurences, dates):
-    for user in tab_tot_occurences:
+def add_first_last_dates(tab_tot_occurrences, dates):
+    for user in tab_tot_occurrences:
         date_eventi = dates[user]
-        tab_tot_occurences[user]['last_date'] = date_eventi[0]
-        tab_tot_occurences[user]['first_date'] = date_eventi[-1]
-    return tab_tot_occurences
+        tab_tot_occurrences[user]['last_date'] = date_eventi[0]
+        tab_tot_occurrences[user]['first_date'] = date_eventi[-1]
+    return tab_tot_occurrences
                         
 
 filein = input('insert path of the json file to analyze:')
 pos = filein.rfind('/')
 nomefile = filein[pos:]
 log_list = read_json_file(filein)
-tab_tot_occurences = tot_occurrences(log_list)
-tab_tot_occurences = event_occurences(log_list, tab_tot_occurences)
-dates = dates_of_occurences(tab_tot_occurences)
-tab_tot_occurences = x(tab_tot_occurences, dates)
-write_json_file(tab_tot_occurences, 'outdata' + nomefile, indnt=3)
+tab_tot_occurrences = add_tot_occurrences(log_list)
+tab_tot_occurrences = add_event_occurrences(log_list, tab_tot_occurrences)
+dates = get_dates_of_occurrences(tab_tot_occurrences)
+tab_tot_occurrences = add_first_last_dates(tab_tot_occurrences, dates)
+write_json_file(tab_tot_occurrences, 'outdata' + nomefile, indnt=3)
 
 print('Fine')
